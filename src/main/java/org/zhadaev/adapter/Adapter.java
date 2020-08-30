27a777e1d18973c6fb7d28c1f10ef2353c6d13e7
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-08-30 16:01:10 +0400

    project created

---
+++ src/main/java/org/zhadaev/adapter/Adapter.java
@@ -0,0 +1,13 @@
+package org.zhadaev.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adapter {
+
+    public static void main(String... args) {
+        SpringApplication.run(Adapter.class);
+    }
+
+}
